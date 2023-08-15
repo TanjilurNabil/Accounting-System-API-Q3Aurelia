a427d34ad1f9632d217d9942e4e502f62c5c9f79
Tanjilur Rahman Nabil <tanjilur.nabil@savethechildren.org>
2023-08-15 12:31:15 +0600

    Missing Column added

--- AccountingSystemAPI.DataAccess/Migrations/20230815062951_OrderFixed.Designer.cs
+++ AccountingSystemAPI.DataAccess/Migrations/20230815062951_OrderFixed.Designer.cs
@@ -4,6 +4,7 @@ using AccountingSystemAPI.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AccountingSystemAPI.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230815062951_OrderFixed")]
+    partial class OrderFixed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AccountingSystemAPI.DataAccess/Migrations/20230815062951_OrderFixed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountingSystemAPI.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SellingUnit",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SellingUnit",
+                table: "Orders");
+        }
+    }
+}
